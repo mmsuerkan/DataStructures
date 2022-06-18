eb7f26790edd52832687a5184c5baab926ca9d36
Mustafa Mert SÜERKAN <msuerkan@partner.innova.com.tr>
2022-06-18 19:28:37 +0300

    Search in a linked List

---
+++ src/main/java/linkedList/challange2/SearchInASingleLinkedList.java
@@ -0,0 +1,18 @@
+package linkedList.challange2;
+
+import linkedList.SinglyLinkedList;
+
+public class SearchInASingleLinkedList {
+
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        list.insertAtEnd(1);
+        list.insertAtEnd(2);
+        list.insertAtEnd(3);
+        list.insertAtEnd(4);
+        list.insertAtEnd(5);
+
+        System.out.println(list.searchNode(4));
+        System.out.println(list.searchNode(6));
+    }
+}
